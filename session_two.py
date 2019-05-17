a3acaa0ead1b988b4add4a048cfab38bbe4410d9
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-05-17 23:54:16 +0100

    did some more exercice

--- session_two.py
+++ session_two.py
@@ -2,44 +2,89 @@
 # Exercice 1
 
 miles = 100
-kilometers = miles * 1.609
-print(kilometers)
+# kilometers = miles * 1.609
+# print(kilometers)
 
-# Area of rectangular room
-# Exercice 2
+# # Area of rectangular room
+# # Exercice 2
 
-length = 3
-width = 4
-area = length * width
-print(area)
+# length = 3
+# width = 4
+# area = length * width
+# print(area)
 
-# Dynamic Area
-# Exercice 3
+# # Dynamic Area
+# # Exercice 3
 
-length_from_input = float(input("Enter length"))
-width_from_input = float(input("Enter width"))
-area_from_user = length_from_input * width_from_input
-print('The area of the squared room of length', length_from_input, 'and width' , width_from_input, 'is', area_from_user)
+# length_from_input = float(input('Enter length'))
+# width_from_input = float(input('Enter width'))
+# area_from_user = length_from_input * width_from_input
+# print('The area of the squared room of length', length_from_input, 'and width' , width_from_input, 'is', area_from_user)
 
-# computing Loan payment
-# Exercice 4
+# # computing Loan payment
+# # Exercice 4
 
-annual_interest_rate = float(input("Enter annual interest rate, e.g., 8.25: "))
-monthly_interest_rate  = annual_interest_rate / 1200
+# annual_interest_rate = float(input('Enter annual interest rate: '))
+# monthly_interest_rate  = annual_interest_rate / 1200
 
-number_of_years = float(input("Enter number of years as an integer, e.g., 5: "))
+# number_of_years = float(input('Enter number of years as an integer: '))
 
-loan_amount = float(input("Enter loan amount, e.g., 120000.95: "))
+# loan_amount = float(input('Enter loan amount: '))
 
-monthly_payment = loan_amount * monthly_interest_rate / (1
-  - 1 / (1 + monthly_interest_rate) ** (number_of_years * 12))
-total_payment = monthly_payment * number_of_years * 12
+# monthly_payment = loan_amount * monthly_interest_rate / (1
+#   - 1 / (1 + monthly_interest_rate) ** (number_of_years * 12))
+# total_payment = monthly_payment * number_of_years * 12
 
 
-print("The monthly payment is " + str(int(monthly_payment * 100) / 100))
-print("The total payment is " + str(int(total_payment * 100) /100))
+# print('The monthly payment is' + str(int(monthly_payment * 100) / 100))
+# print('The total payment is' + str(int(total_payment * 100) /100))
 
-# Password check
-# Exercice 5
+# # Password check
+# # Exercice 5
+
+# password = 'ILoveCats'
+# login = 'ChubbyCat01'
+
+# user_login = input('login?')
+# user_password = input('password?')
+
+# if login == str(user_login) and password == str(user_password):
+# 	print('Acces granted')
+# else :
+# 	print('Denied')
+
+# Check number divisor
+# Exercice 6
+
+# user_number = input('Please enter a number:')
+
+# if int(user_number)% 5  == 0 :
+# 	print('HiFive')
+# elif int(user_number)% 2 == 0 :
+# 	print('hiEven')
+# else:
+# 	print('nop')
+
+# Grading Students
+# Exercice 7
+
+# student_grade = input('Enter student grade')
+
+# if student_grade >= 90:
+# 	print('A')
+# elif student_grade < 90 and student_grade >= 80:
+# 	print('B')
+# elif student_grade < 80 and student_grade >= 70:
+# 	print('C')
+# elif student_grade < 70 and student_grade >= 60:
+# 	print('D')
+# elif student_grade < 60:
+# 	print('D')
+# else:
+# 	print('impossible value')
+
+# determining leap year
+# Exercice 8
+
 
 
