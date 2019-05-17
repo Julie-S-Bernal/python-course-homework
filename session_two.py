bb3f749790b861c891d17b43207718cb35a02965
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-05-17 22:59:14 +0100

    did the first 3 exercice

---
+++ session_two.py
@@ -0,0 +1,23 @@
+# Algorythm to python code:
+# Exercice 1
+
+miles = 100
+kilometers = miles * 1.609
+print(kilometers)
+
+# Area of rectangular room
+# Exercice 2
+
+length = 3
+width = 4
+area = length * width
+print(area)
+
+# Dynamic Area
+# Exercice 3
+
+length_from_input = float(input("Enter length"))
+width_from_input = float(input("Enter width"))
+area_from_user = length_from_input * width_from_input
+print("The area of the squared room of length:" length_from_input "and width : "  width_from_input, "is :", area_from_user)
+
